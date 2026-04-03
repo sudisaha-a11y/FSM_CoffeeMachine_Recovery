0d643d818e2fc3f7fae00e284a1922ae9ffe7b58
sudisaha-a11y <sudi.saha@gmail.com>
2026-04-03 18:18:08 +0530

    Add configuration settings for the coffee machine

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+"""
+Configuration settings for the coffee machine.
+"""
+
+# Coffee machine settings
+BREW_TIME = 300  # seconds
+MAX_TEMPERATURE = 95  # Celsius
+MIN_TEMPERATURE = 70  # Celsius
+WATER_LEVEL_WARNING = 0.2  # 20%
