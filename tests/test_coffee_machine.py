01943ca4fe7abcb1499f1a9afd83d21aa4a0371a
sudisaha-a11y <sudi.saha@gmail.com>
2026-04-03 18:17:24 +0530

    Add unit tests for coffee machine module

---
+++ tests/test_coffee_machine.py
@@ -0,0 +1,23 @@
+"""
+Unit tests for the coffee machine module.
+"""
+
+import unittest
+
+class TestCoffeeMachine(unittest.TestCase):
+    """Test cases for the coffee machine."""
+
+    def test_initialize(self):
+        """Test machine initialization."""
+        pass
+
+    def test_start(self):
+        """Test machine start."""
+        pass
+
+    def test_stop(self):
+        """Test machine stop."""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
